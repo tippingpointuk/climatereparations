51beb0f7aa0a64b9d4894f60bc601ab7d5085174
joe-irving <joeirving.jbi@gmail.com>
2021-10-06 12:35:54 +0100

    Update tagline

--- assets/js/actions-map.js
+++ assets/js/actions-map.js
@@ -53,7 +53,9 @@ var titleLayer = L.tileLayer(
   }
 ).addTo(actionsMap);
 
-var markerCluster = L.markerClusterGroup();
+var markerCluster = L.markerClusterGroup({
+  maxClusterRadius: 40,
+});
 
 for (i in actionsData["events"]){
   var markerLocation = actionsData["events"][i]["location"]["location"];
